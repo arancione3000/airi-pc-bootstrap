7717f8b642478576e6d4cb0bd39c30df234f3501
arancione3000 <130790689+arancione3000@users.noreply.github.com>
2026-09-21 22:27:29 +0200

    android: bump meta-evolution observability release to v1.4.0

--- airi-generalist-android/app/build.gradle.kts
+++ airi-generalist-android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.airipc.generalist"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
